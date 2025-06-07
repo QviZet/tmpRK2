45d5a0aee15b6d87fd72cb8600efa9ac2e5c8400
QviZet <mgv638@mail.ru>
2025-06-07 14:43:18 +0300

    Update tests.cpp

--- testing/tests.cpp
+++ testing/tests.cpp
@@ -1,39 +1,10 @@
 #include "Header.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-void ConcretePrototype1::print()
-{
-    cout << "concrete 1: " << _a << endl;
-}
-
-ConcretePrototype1::ConcretePrototype1() {};
-
-ConcretePrototype1::ConcretePrototype1(int a) :_a(a) {};
-
-ConcretePrototype1* ConcretePrototype1::clone()
-{
-    return new ConcretePrototype1(*this);
-}
-
-void ConcretePrototype2::print()
-{
-    cout << "concrete 2: " << _a << endl;
-}
-
-ConcretePrototype2::ConcretePrototype2() {};
-
-ConcretePrototype2::ConcretePrototype2(int a) :_a(a) {};
-
-ConcretePrototype2* ConcretePrototype2::clone()
-{
-    return new ConcretePrototype2(*this);
-}
-
-PrototypeCreator::PrototypeCreator(){
-    _protoList[Type::Concrete1] = new ConcretePrototype1(12);
-    _protoList[Type::Concrete2] = new ConcretePrototype2(23);
-}
-
-Prototype* PrototypeCreator::createPrototype(Type t)
-{
-    return _protoList[t]->clone();
+int main(int argc, char** argv) {
+    ::testing::FLAGS_gmock_verbose = "error";
+	::testing::InitGoogleMock(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS();
 }
