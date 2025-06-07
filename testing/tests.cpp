f620a144c635cbaec7cb52b32f56584229ee3458
QviZet <mgv638@mail.ru>
2025-06-07 19:56:43 +0300

    Update tests.cpp

--- testing/tests.cpp
+++ testing/tests.cpp
@@ -2,6 +2,23 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+using ::testing::_;
+
+class mockCon1 : public ConcretePrototype1 {
+public:
+    MOCK_METHOD(void, print, (), (override));
+};
+
+class mockCon2 : public ConcretePrototype2 {
+public:
+    MOCK_METHOD(void, print, (), (override));
+};
+
+class mockCreator : public PrototypeCreator {
+public:
+    MOCK_METHOD(Prototype*, createPrototype, (Type t), (override));
+};
+
 int main(int argc, char** argv) {
     ::testing::FLAGS_gmock_verbose = "error";
 	::testing::InitGoogleMock(&argc, argv);
