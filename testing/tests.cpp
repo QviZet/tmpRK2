9b771d90d4c9e1ba77574f1c45504e814832ab0d
QviZet <mgv638@mail.ru>
2025-06-07 20:51:06 +0300

    Update tests.cpp

--- testing/tests.cpp
+++ testing/tests.cpp
@@ -19,9 +19,23 @@ public:
     MOCK_METHOD(Prototype*, createPrototype, (Type t), ());
 };
 
+//......................................gtest......................................
+
+TEST(ConcretePrototype1, clone){
+	ConcretePrototype1* newP = new ConcretePrototype1(10);
+	Prototype* test2 = newP->clone();
+	EXPECT_EQ("concrete 1: 10", newP.print());
+}
+
+TEST(ConcretePrototype2, clone){
+	ConcretePrototype2* newP = new ConcretePrototype2(10);
+	Prototype* test2 = newP->clone();
+	EXPECT_EQ("concrete 1: 10", newP.print());
+}
+
 int main(int argc, char** argv) {
-    ::testing::FLAGS_gmock_verbose = "error";
+	::testing::FLAGS_gmock_verbose = "error";
 	::testing::InitGoogleMock(&argc, argv);
-    ::testing::InitGoogleMock(&argc, argv);
+	::testing::InitGoogleMock(&argc, argv);
 	return RUN_ALL_TESTS();
 }
